b5a94b0036071c0b800d88edd625de50fa20139d
João Pedro <joaopedroredondo@hotmail.com>
2025-01-23 00:27:52 -0300

    Adicionando dependencias

---
+++ pagamementos/src/main/java/br/com/techfoods/pagamementos/PagamementosApplication.java
@@ -0,0 +1,13 @@
+package br.com.techfoods.pagamementos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamementosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamementosApplication.class, args);
+	}
+
+}
---
+++ pagamementos/src/test/java/br/com/techfoods/pagamementos/PagamementosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techfoods.pagamementos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamementosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
